94ea3897ff158b446e5a25c3fe04f372595f1cfb
DanilMir <mirgayazov02@gmail.com>
2023-11-10 11:35:13 +0300

    feat(auth): add auth service

--- src/Auth/Program.cs
+++ src/Auth/Program.cs
@@ -1,6 +1,125 @@
+using Auth;
+using Auth.Data;
+using Microsoft.EntityFrameworkCore;
+using static OpenIddict.Abstractions.OpenIddictConstants;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    // Configure Entity Framework Core to use Microsoft SQL Server.
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        optionsBuilder => { optionsBuilder.EnableRetryOnFailure(10, TimeSpan.FromSeconds(30), null); }
+    );
+
+    // Register the entity sets needed by OpenIddict.
+    // Note: use the generic overload if you need to replace the default OpenIddict entities.
+    options.UseOpenIddict();
+});
+
+builder.Services
+    .AddDefaultIdentity<AuthUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddRazorPages();
+
+
+builder.Services.AddOpenIddict()
+
+    // Register the OpenIddict core components.
+    .AddCore(options =>
+    {
+        // Configure OpenIddict to use the Entity Framework Core stores and models.
+        // Note: call ReplaceDefaultEntities() to replace the default entities.
+        options.UseEntityFrameworkCore()
+            .UseDbContext<ApplicationDbContext>();
+    })
+
+    // Register the OpenIddict server components.
+    .AddServer(options =>
+    {
+        // Enable the authorization, logout, token and userinfo endpoints.
+        options
+            .SetAuthorizationEndpointUris("connect/authorize")
+            .SetDeviceEndpointUris("connect/device")
+            .SetVerificationEndpointUris("connect/verify")
+            .SetLogoutEndpointUris("connect/logout")
+            .SetTokenEndpointUris("connect/token")
+            .SetUserinfoEndpointUris("connect/userinfo");
+
+        // Mark the "email", "profile" and "roles" scopes as supported scopes.
+        options.RegisterScopes(Scopes.Email, Scopes.Profile, Scopes.Roles);
+
+        // Enable necessary flows
+        options.AllowClientCredentialsFlow();
+        options.AllowDeviceCodeFlow();
+        options.AllowAuthorizationCodeFlow();
+        options.AllowRefreshTokenFlow();
+
+        // Register the signing and encryption credentials.
+        options.AddDevelopmentEncryptionCertificate()
+            .AddDevelopmentSigningCertificate();
+
+        // Register the ASP.NET Core host and configure the ASP.NET Core-specific options.
+        options.UseAspNetCore()
+            .EnableAuthorizationEndpointPassthrough()
+            .EnableLogoutEndpointPassthrough()
+            .EnableTokenEndpointPassthrough()
+            .EnableUserinfoEndpointPassthrough()
+            .EnableVerificationEndpointPassthrough()
+            .EnableStatusCodePagesIntegration();
+    })
+
+    // Register the OpenIddict validation components.
+    .AddValidation(options =>
+    {
+        // Import the configuration from the local OpenIddict server instance.
+        options.UseLocalServer();
+
+        // Register the ASP.NET Core host.
+        options.UseAspNetCore();
+    });
+
+// Register the worker responsible of seeding the database with the sample clients.
+// Note: in a real world application, this step should be part of a setup script.
+builder.Services.AddHostedService<Worker>();
+
+
+// CORS policy to allow SwaggerUI and React clients
+// builder.Services.AddCors(
+//     options =>
+//     {
+//         options.AddPolicy(
+//             "default", policy =>
+//             {
+//                 policy
+//                     .WithOrigins(
+//                         builder.Configuration.GetServiceUri("weather-api")!.ToString().TrimEnd('/'),
+//                         builder.Configuration.GetServiceUri("weather-summary-api")!.ToString().TrimEnd('/'),
+//                         builder.Configuration.GetServiceUri("react-client")!.ToString().TrimEnd('/'))
+//                     .AllowAnyHeader()
+//                     .AllowAnyMethod();
+//             });
+//     });
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// app.UseDeveloperExceptionPage();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+
+app.UseRouting();
+// app.UseCors("default");
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapDefaultControllerRoute();
+app.MapRazorPages();
 
 app.Run();
