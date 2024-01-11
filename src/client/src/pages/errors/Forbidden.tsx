542e79a928c5365f3a4cc143df779747b840cd4e
flowXM <fusion.flowx@gmail.com>
2024-01-11 14:44:52 +0300

    feat(client): add some status code pages

--- src/client/src/pages/errors/Forbidden.tsx
+++ src/client/src/pages/errors/Forbidden.tsx
@@ -1,14 +1,17 @@
+import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
 
-export default function NotFound() {
+export default function Forbidden() {
     return (
         <>
             <Container>
                 <Box mt={5}>
                     <Typography variant="h4" color="error">
-                        404 NotFound
+                        403 Forbidden
+                    </Typography>
+                    <Typography variant="body1">
+                        Access to this resource is forbidden. Please contact the administrator for assistance.
                     </Typography>
                 </Box>
             </Container>
