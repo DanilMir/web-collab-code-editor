01951d68456b907745c73307da286667bffeef2c
flowXM <fusion.flowx@gmail.com>
2024-04-21 20:42:09 +0300

    feat: add dotnet-gtk dockerfile

--- src/Sandbox/Scripts/build-all.sh
+++ src/Sandbox/Scripts/build-all.sh
@@ -1,2 +1,3 @@
 docker build -f Dockerfile.Base -t sandbox-base .
-docker build -f Dockerfile.Dotnet.Console -t sandbox-dotnet-console .
+docker build -f Dockerfile.Dotnet -t sandbox-dotnet .
+docker build -f Dockerfile.Dotnet.GTK -t sandbox-dotnet-gtk .
--- src/Sandbox/Scripts/startapp.sh
+++ src/Sandbox/Scripts/startapp.sh
@@ -16,12 +16,10 @@ chmod 600 ~/.vnc/passwd
 
 # rm -rfv /tmp/.X*-lock /tmp/.X11-unix
 
-
 openrc boot > openrc.log 2>&1
 ln -s vncserver /etc/init.d/vncserver.1
 rc-service vncserver.1 start > vncserver.log 2>&1
 websockify -D --web /usr/share/novnc/ 6901 localhost:5901 > websockify.log 2>&1  
-
 
 DISPLAY=:1 exec "$@" > exec.log 2>&1
 
